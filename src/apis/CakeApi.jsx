4d1a1c25ff8acc8e34e4c5716f83d31e72aebdc1
larzarus0320 <astar5327z@gmail.com>
2024-06-02 23:45:18 +0900

    test: cake api 연결 테스트

--- src/apis/CakeApi.jsx
+++ src/apis/CakeApi.jsx
@@ -1,17 +1,22 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const CakeAPI = async () => {
   const SERVER_URL = process.env.REACT_APP_SERVER_URL;
-  const API_TOKEN = process.env.REACT_APP_API_TOKEN;
+  const API_TOKEN = localStorage.getItem('accessToken');
 
-  if (!SERVER_URL || !API_TOKEN) {
-    throw new Error("SERVER_URL or API_TOKEN is not defined");
+  if (!SERVER_URL) {
+    throw new Error('SERVER_URL is not defined');
   }
 
   try {
     const response = await axios.get(`${SERVER_URL}/cakes/view`, {
       headers: {
         Authorization: `Bearer ${API_TOKEN}`,
+      },
+      // TODO: 아래는 테스트 params이며, 실제로는 동적으로 유저가 입력한 값을 요청해야함.
+      params: {
+        email: 'test1234@gmail.com',
+        createdYear: 2024,
       },
     });
 
@@ -21,10 +26,10 @@ export const CakeAPI = async () => {
     if (error.response && error.response.data) {
       throw new Error(
         error.response.data.message ||
-          "An error occurred while fetching the data."
+          'An error occurred while fetching the data.'
       );
     } else {
-      throw new Error("An error occurred while fetching the data.");
+      throw new Error('An error occurred while fetching the data.');
     }
   }
 };
